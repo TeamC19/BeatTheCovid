5ab15d41ad3c0a24336d457006ff66ba2b1951d4
Javi <jaaiar@inf.upv.es>
2021-04-01 21:56:32 +0200

    Modo combate

--- Beat the COVID/Assets/Scripts/GameEngine.cs
+++ Beat the COVID/Assets/Scripts/GameEngine.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class GameEngine : MonoBehaviour
 {
     public CameraController cameraController;
+    public GameObject cinemachine;
+    public GameObject cinemachine2;
     public Animator cameraAnimator;
     public Transform background;
+    public bool fightMode = false;
     public float val = 0;
     public float speed = 0.1f;
     // Start is called before the first frame update
@@ -33,6 +37,26 @@ public class GameEngine : MonoBehaviour
             val -= speed;
             print("PULSADO ABAJO val: " + val);
         }
-        
+
+
+        // Modo combate, por ahora se activa con espacio, pero lo suyo es que se active en momentos concretos
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!fightMode)
+            {
+                cinemachine.SetActive(true);
+                cinemachine.transform.position = cinemachine2.transform.position + Vector3.up*1.6f+Vector3.right*1f;
+               cinemachine2.SetActive(false);
+                print("Modo combate activado");
+                fightMode = true;
+            }
+            else
+            {
+                cinemachine.SetActive(false);
+                cinemachine2.SetActive(true);
+                fightMode = false;
+                print("Modo combate desactivado");
+            }
+        }
     }
 }
