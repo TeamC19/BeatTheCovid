4cd8ab88cb514bca0bf32de7e838349c0ca9f218
Javi <jaaiar@inf.upv.es>
2021-04-01 21:26:37 +0200

    Añadido Cinemachine para controlar el movimiento en horizontal y colliders a los lados para los momentos de cámara fija

---
+++ Beat the COVID/Assets/Scripts/LockYaxisCinemachine.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+/// <summary>
+/// An add-on module for Cinemachine Virtual Camera that locks the camera's Z co-ordinate
+/// </summary>
+[ExecuteInEditMode]
+[SaveDuringPlay]
+[AddComponentMenu("")] // Hide in menu
+public class LockYaxisCinemachine : CinemachineExtension
+{
+    [Tooltip("Lock the camera's Z position to this value")]
+    public float yPosition = 2.33f;
+
+    protected override void PostPipelineStageCallback(
+        CinemachineVirtualCameraBase vcam,
+        CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (stage == CinemachineCore.Stage.Body)
+        {
+            var pos = state.RawPosition;
+            pos.y = yPosition;
+            state.RawPosition = pos;
+        }
+    }
+}
