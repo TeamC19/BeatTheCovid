9552e3d1952222fd5605adc3399ecb66f45bfd7c
Nieves <35975813+niecogi@users.noreply.github.com>
2021-04-03 19:32:13 +0200

    Implemented animation run

--- Beat the COVID/Assets/Scripts/PlayerController.cs
+++ Beat the COVID/Assets/Scripts/PlayerController.cs
@@ -5,37 +5,62 @@ using UnityEngine;
 public class PlayerController: MonoBehaviour
 {
     public float speed = 1f;
-    private SpriteRenderer sprite;
+    private SpriteRenderer _sprite;
+    private Animator _anim;
+    
     // Start is called before the first frame update
     void Start()
     {
-        sprite = GetComponent<SpriteRenderer>();   
+        _sprite = GetComponent<SpriteRenderer>();
+        _anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float deltaX;
+        if (Input.GetKey(KeyCode.Space) && transform.position.y == 0) {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+            _anim.SetBool("IsJumping",true);
+        }else
         if (Input.GetKey(KeyCode.W) )
         {
+                deltaX = 1f;
                 transform.Translate(Vector3.up * Time.deltaTime * speed);
+                _anim.SetFloat("speed", Mathf.Abs(deltaX));
         }
-
+        else 
         if (Input.GetKey(KeyCode.S))
         {
-                transform.Translate(Vector3.down * Time.deltaTime * speed);
-        }
+            deltaX = 1f;
+
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+            _anim.SetFloat("speed", Mathf.Abs(deltaX));
+        }else
 
         if (Input.GetKey(KeyCode.D))
         {
-            sprite.flipX = false;
+            deltaX = 1f;
+            _sprite.flipX = false;
             transform.Translate(Vector3.right * Time.deltaTime * speed);
-        }
+            _anim.SetFloat("speed", Mathf.Abs(deltaX));
+        }else
 
         if (Input.GetKey(KeyCode.A))
         {
-            sprite.flipX = true;
+            deltaX = 1f;
+            _sprite.flipX = true;
             transform.Translate(Vector3.left * Time.deltaTime * speed);
+            _anim.SetFloat("speed", Mathf.Abs(deltaX));
+        }else
+        {
+            deltaX = 0f;
+            _anim.SetFloat("speed", Mathf.Abs(deltaX));
         }
+       
+            
+        
+       
     }
 
     
