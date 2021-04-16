0982a17cd9bc116003cf4097da2667fcdd95ae2b
Javi <jaaiar@inf.upv.es>
2021-04-16 15:37:25 +0200

    Lanzamiento de vacunas completado

--- Beat the COVID/Assets/Scripts/InjectionController.cs
+++ Beat the COVID/Assets/Scripts/InjectionController.cs
@@ -7,13 +7,16 @@ public class InjectionController : MonoBehaviour
     [SerializeField] float speed = 4;
     // public variables because they're changed by the player
     public bool thrown;
+   // public bool exploded;
     public float y;
     [SerializeField] Vector3 LaunchOffset;
     [SerializeField] float despawnTime = 5;
     private Rigidbody2D rb2d;
     private SpriteRenderer spriteRenderer;
+    [SerializeField] LayerMask enemyLayer;
+    [SerializeField] float attackRange = 2.55f;
     [SerializeField] Sprite smoke;
-     
+
 
     void Start()
     {
@@ -25,13 +28,12 @@ public class InjectionController : MonoBehaviour
             var direction = transform.localScale.x * transform.right + Vector3.up;
             rb2d.AddForce(direction * speed, ForceMode2D.Impulse);
 
-            print(y);
             Destroy(gameObject, despawnTime);
         } else
         {
             transform.eulerAngles = Vector3.forward * Random.Range(0, 360);
         }
-        
+
     }
 
     private void Update()
@@ -41,12 +43,29 @@ public class InjectionController : MonoBehaviour
             transform.position += transform.localScale.x * transform.right * speed * Time.deltaTime;
             if (transform.position.y <= y)
             {
-                rb2d.simulated = false;
+                var direction = transform.localScale.x * transform.right + Vector3.up;
+                rb2d.velocity = Vector3.zero;
+                rb2d.isKinematic = true;
                 thrown = false;
                 spriteRenderer.sprite = smoke;
-                Destroy(gameObject, 0.5f);
+                DamageInArea();
+                // Destroy(gameObject, 0.5f);
             }
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      
+    }
+
+    public void DamageInArea() {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, attackRange, enemyLayer);
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            // Hacer daño
+        }
+    }
+
 
 }
