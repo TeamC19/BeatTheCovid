c9cb87f5641174b1d5ff5f543eb83327816e1a9f
Cristina Padró <34605397+SilverArrow23@users.noreply.github.com>
2021-04-21 13:29:49 +0200

    Added 2nd level boss scene added and functional main menu

---
+++ Beat the COVID/Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Function to load next level when we hit play
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Level3");
+        // The correct code would be:
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // But since we don't have all the levels, we will load only Level 3
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game");
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+}
