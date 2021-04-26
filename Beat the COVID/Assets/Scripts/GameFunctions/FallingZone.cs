1588506b5372bf842ae06953d7ba800a020cc54d
Javi <jaaiar@inf.upv.es>
2021-04-27 01:46:10 +0200

    Borrados mensajes de debug

--- Beat the COVID/Assets/Scripts/GameFunctions/FallingZone.cs
+++ Beat the COVID/Assets/Scripts/GameFunctions/FallingZone.cs
@@ -6,7 +6,6 @@ public class FallingZone : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("gund");
         if (collision.CompareTag("Player"))
         {
             PlayerController player = collision.GetComponent<PlayerController>();
