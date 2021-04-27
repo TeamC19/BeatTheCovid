2398401a2300218e9c11853e0570be4d3b26f042
angar44a <antoni.galardo2000@gmail.com>
2021-04-27 12:58:36 +0200

    Transicion escena de nivel a boss

---
+++ Beat the COVID/Assets/Scripts/UI/PlayerEnterTransition.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerEnterTransition : MonoBehaviour
+{
+    // Reference to animator for scene transition
+    public Animator _transition;
+    // Time it takes to wait for animation to end
+    public float transitionTime = 1f;
+    public void StartGame()
+    {
+        // Load the next scene, with menu unpaused
+        StartCoroutine(LoadGameLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        Time.timeScale = 1f;
+        PauseMenu.GameIsPaused = false;
+    }
+
+    // Coroutine to play transition animation to go to either play level or boss level
+    IEnumerator LoadGameLevel(int levelIndex)
+    {
+        // Play transition animation
+        _transition.SetTrigger("Start");
+
+        // Wait for transition animation to end
+        yield return new WaitForSeconds(transitionTime);
+
+        // Load next scene
+        SceneManager.LoadScene(levelIndex);
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")) StartGame();
+    }
+
+}
